48922f727f11817a0d33b2bec1527a64a1e20e44
Timothy Justin Oesch <54284649+timothyoesch@users.noreply.github.com>
2023-06-07 20:24:32 +0200

    Add data protection policies for other organizations

--- config/sr.php
+++ config/sr.php
@@ -10,5 +10,45 @@ return [
             Je suis d'accord que le JSS me tienne au courant. <a class="underline" href="https://juso.ch/de/datenschutz/" target="_blank">Plus d'informations dans notre politique de confidentialité</a>.
             EOD,
         ],
+        "juso" => [
+            "de" => <<<EOD
+            Ich bin einverstanden, dass mich die JUSO auf dem Laufenden hält. <a class="underline" href="https://juso.ch/de/datenschutz/" target="_blank">Mehr dazu in unseren Datenschutzbestimmungen</a>.
+            EOD,
+            "fr" => <<<EOD
+            Je suis d'accord que le JSS me tienne au courant. <a class="underline" href="https://juso.ch/de/datenschutz/" target="_blank">Plus d'informations dans notre politique de confidentialité</a>.
+            EOD,
+        ],
+        "campax" => [
+            "de" => <<<EOD
+            Ich bin einverstanden, dass mich Campax auf dem Laufenden hält. <a class="underline" href="https://campax.org/datenschutzrichtlinien/" target="_blank">Mehr dazu in unseren Datenschutzbestimmungen</a>.
+            EOD,
+            "fr" => <<<EOD
+            Je suis d'accord que Campax me tienne au courant. <a class="underline" href="https://campax.org/fr/datenschutzrichtlinien/" target="_blank">Plus d'informations dans notre politique de confidentialité</a>.
+            EOD,
+        ],
+        "spf" => [
+            "de" => <<<EOD
+            Ich bin einverstanden, dass mich die SP Frauen auf dem Laufenden hält. <a class="underline" href="https://www.sp-ps.ch/datenschutz-policy/" target="_blank">Mehr dazu in unseren Datenschutzbestimmungen</a>.
+            EOD,
+            "fr" => <<<EOD
+            Je suis d'accord que Campax me tienne au courant. <a class="underline" href="https://www.sp-ps.ch/fr/politique-de-protection-des-donnees/" target="_blank">Plus d'informations dans notre politique de confidentialité</a>.
+            EOD,
+        ]
+        "brava" => [
+            "de" => <<<EOD
+            Ich bin einverstanden, dass mich BRAVA auf dem Laufenden hält. <a class="underline" href="https://www.brava-ngo.ch/de/datenschutz" target="_blank">Mehr dazu in unseren Datenschutzbestimmungen</a>.
+            EOD,
+            "fr" => <<<EOD
+            Je suis d'accord que BRAVA me tienne au courant. <a class="underline" href="https://www.brava-ngo.ch/fr/protections-des-donnees" target="_blank">Plus d'informations dans notre politique de confidentialité</a>.
+            EOD,
+        ],
+        "cfd" => [
+            "de" => <<<EOD
+            Ich bin einverstanden, dass mich der cfd auf dem Laufenden hält. <a class="underline" href="https://www.cfd-ch.org/de/agbs-und-datenschutz-140.html" target="_blank">Mehr dazu in unseren Datenschutzbestimmungen</a>.
+            EOD,
+            "fr" => <<<EOD
+            Je suis d'accord que le cfd me tienne au courant. <a class="underline" href="https://www.cfd-ch.org/de/agbs-und-datenschutz-140.html" target="_blank">Plus d'informations dans notre politique de confidentialité</a>.
+            EOD,
+        ]
     ]
 ];
