44adb46e750369e19a1f536f6bf3444afc655769
Timothy Justin Oesch <54284649+timothyoesch@users.noreply.github.com>
2023-06-08 18:04:31 +0200

    Add Commands

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('supporters:resend-verification-emails')->dailyAt('10:00');
     }
 
     /**
